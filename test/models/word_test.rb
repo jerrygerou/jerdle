89a215ae7cffee874f98d2d96dcd622312b155c2
jerrygerou <jerrysgerou@gmail.com>
2022-07-07 14:17:43 -0500

    Model tests for validation of Word

--- test/models/word_test.rb
+++ test/models/word_test.rb
@@ -1,7 +1,22 @@
 require "test_helper"
 
 class WordTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "word will not create if body is nil" do
+    word = Word.create(body: nil)
+    assert_equal false, word.valid?
+  end
+
+  test "word will not create if body length is not equal to 5" do
+    word = Word.create(body: "longer")
+    word_two = Word.create(body: "less")
+
+    assert_equal false, word.valid?
+    assert_equal false, word_two.valid?
+  end
+
+  test "word will create if body exists and has length equal to 5" do
+    word = Word.create(body: "mambo")
+    assert word.valid?
+  end
 end
